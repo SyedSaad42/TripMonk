9edd5f5dfb06fff481a9a982e55a8e609c2b79a6
syed saad qadeer <saadsyed454@gmail.com>
2025-04-20 10:58:56 -0400

    Started with the frontend

---
+++ frontend_/src/component/Navbar.jsx
@@ -0,0 +1,18 @@
+import { React } from "react"
+
+const Navbar = ()=>{
+    return(
+//icon
+<div style={{display:"inline"}} >
+    <img src={'https://images.pexels.com/photos/416974/pexels-photo-416974.jpeg?auto=compress&cs=tinysrgb&w=600'} alt="Icon" style={{width: "50px", height: "50px"}} />
+    <span style={{marginLeft: "10px"}}>TripMonk</span>
+</div>
+
+// name of compony
+    
+
+    )
+
+}
+
+export default Navbar;
